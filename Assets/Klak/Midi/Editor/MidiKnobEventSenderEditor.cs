42ab083eaeab57ccf96cfed7e11db3b2ab0fd409
Keijiro Takahashi <keijiro@ZX.local>
2016-01-21 21:55:00 +0900

    Add editor script for MidiKnobEventSender.

---
+++ Assets/Klak/Midi/Editor/MidiKnobEventSenderEditor.cs
@@ -0,0 +1,58 @@
+//
+// MidiKlak - MIDI extension for Klak
+//
+// Copyright (C) 2016 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Midi
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(MidiKnobEventSender))]
+    public class MidiKnobEventSenderEditor : Editor
+    {
+        SerializedProperty _channel;
+        SerializedProperty _knobNumber;
+        SerializedProperty _interpolator;
+        SerializedProperty _knobEvent;
+
+        void OnEnable()
+        {
+            _channel = serializedObject.FindProperty("_channel");
+            _knobNumber = serializedObject.FindProperty("_knobNumber");
+            _interpolator = serializedObject.FindProperty("_interpolator");
+            _knobEvent = serializedObject.FindProperty("_knobEvent");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(_channel);
+            EditorGUILayout.PropertyField(_knobNumber);
+            EditorGUILayout.PropertyField(_interpolator);
+            EditorGUILayout.PropertyField(_knobEvent);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
